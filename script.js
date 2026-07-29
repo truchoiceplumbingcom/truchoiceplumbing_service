af28921320eb13995fbeeae25944881c801dc3de
truchoiceplumbingcom <isuwoxipide77@gmail.com>
2026-07-29 23:55:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // =============================
+// LOADER
+// =============================
+
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.remove();
+        }, 400);
+
+    }, 800);
+
+});
+
+// =============================
 // MOBILE MENU
 // =============================
 
